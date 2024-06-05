3d6f0dd1aa08c3d86bb52468c856b4dda6488e14
masaapotat <masaakavulanya@gmail.com>
2024-06-05 17:07:26 +0300

    making the animation for the home page

--- src/features/display/AnimatedDisplayCard.js
+++ src/features/display/AnimatedDisplayCard.js
@@ -1,16 +1,53 @@
+// importing usestate and useeffect from react for animation
+import { useState, useEffect } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { useSpring, animated } from 'react-spring';
 
-const DisplayCard = ({ item }) => {
+const AnimatedDisplayCard = ({ item }) => {
     const { image, name, description } = item;
+    // setting a local state variable named toggle
+
+    
+    const [toggle, setToggle] = useState(false);
+
+
+    // creating a style object with the help of reactSpring and storing in a varible called AnimatedStyle
+
+    const animatedStyle= useSpring({
+        // usespring expects us to pass an object as an argument and the object should describe the kind of animation that we want to happen
+        // if the value of toggle is set to true, we want the opacity 1 whuch is 100%, if the value of opacity os set to false we wan the opacity to be set to 0
+        opacity: toggle ? 1 : 0,
+        // if the value of toggle is set to true, it means the width and height should be  at 100% otherwise 100% width and 0% height
+        transform: toggle? 'scale(1,1)' : 'scale(1,0)',
+        // set a config which itself takes an object since there can be multiple configurations
+        // we want to apply our animation over half a second
+        config: {duration: 500}
+
+        // we want the anmations to apply whenever the display cards components are mounted to the page and not when an update causes them to rerender
+
+       
+    })
+
+     // we call useEffect to handle this side effect
+    //  the first argument is the side effect we want useEffect to run which we pass inside a callback function
+     useEffect(()=>{
+        setToggle(true)
+     },[])
+
     return (
-        <Card>
-            <CardImg src={image} alt={name} />
-            <CardBody>
-                <CardTitle>{name}</CardTitle>
-                <CardText>{description}</CardText>
-            </CardBody>
+        // animatedStyle is the name of the object that we created earlier
+        <animated.div style={animatedStyle}>
+            <Card>
+                <CardImg src={image} alt={name} />
+                    <CardBody>
+                        <CardTitle>{name}</CardTitle>
+                        <CardText>{description}</CardText>
+                    </CardBody>
         </Card>
+
+        </animated.div>
+        
     );
 };
 
-export default DisplayCard;
+export default AnimatedDisplayCard;
--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,5 +1,6 @@
 import { Col, Row } from 'reactstrap';
-import DisplayCard from './DisplayCard';
+// import DisplayCard from './DisplayCard';
+import AnimatedDisplayCard from './AnimatedDisplayCard';
 import { selectFeaturedCampsite } from '../campsites/CampsitesSlice';
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
 import {selectFeaturedPartner} from '../partners/partnersSlice'
@@ -11,9 +12,16 @@ const DisplayList = () => {
         <Row>
             {items.map((item, idx) => {
                 return (
+                    
+                    //     {/* adding conditional rendering incase any of the featured object is set to false, so that it handles the error more gracefully */}
+
+                    // we first check the value of item if one of our selected functions t  select for the featured campsite promotion or partner doesnt find one that is feature it will return undefined which is a falsy value that will cause the && operator to short circuit  and prevent it form rendering anything to its right
+                    item &&(
                     <Col md className='m-1' key={idx}>
-                        <DisplayCard item={item} />
+                    
+                        <AnimatedDisplayCard item={item} />
                     </Col>
+                    )
                 );
             })}
         </Row>
