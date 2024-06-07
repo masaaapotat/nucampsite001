0a58f5761311567159e0d12acafb3e488dcacdb1
masaapotat <masaakavulanya@gmail.com>
2024-06-07 22:52:23 +0300

    validating the email and the phone number ields

--- src/utils/validateContactForm.js
+++ src/utils/validateContactForm.js
@@ -11,6 +11,28 @@ export const validateContactForm = (values) => {
     }else if (values.firstName.length>15) {
         errors.firstName= 'Must be 15 characters or less';
     }
+
+
+    // last name
+    if (!values.lastName) {
+        errors.lastName='Required';
+    }else if (values.lastName.length < 2) {
+        errors.firstNlastNameame= 'Must be at least 2 characters.';
+    }else if (values.lastName.length>15) {
+        errors.lastName= 'Must be 15 characters or less';
+    }
+
+    // regex that checks that from the beginning to the end of the value all parts of the value are made up of digits
+    const reg = /^\d+$/;
+    // test if whatever is in the phone number is anything else than digit
+    if (!reg.test(values.phoneNum)) {
+        errors.phoneNum = 'The phone number should contain only numbers.';
+    }
+    // email
+    // check if the email contains an @
+    if (!values.email.includes('@')) {
+        errors.email = 'Email should contain a @';
+    }
   return (
     <div>validateContactForm</div>
   )
