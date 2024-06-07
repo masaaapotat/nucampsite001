1549a7b378edf7541dcd951f6f18593ada81cac4
masaapotat <masaakavulanya@gmail.com>
2024-06-07 20:48:08 +0300

    addded input fields to form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,76 +1,119 @@
 import { Button, Label, Col, FormGroup } from "reactstrap";
 import { Formik, Field, Form } from "formik";
-
 import React from "react";
 
 const ContactForm = () => {
   return (
     <Formik
-      // {/* defining the top values for the form fields via theinitial values prop that we pass to formik*/}
-      // {/* this prop expects a js object for its value , were defining it as an object literal by adding two sets of {{}}, the outer one signifies that we are about to write js in jsx and the inner signifies that we are writing an object*/}
-      initalValues={{
-        // prpoerties we want to capture in our form
+      // Defining the initial values for the form fields via the initialValues prop that we pass to Formik
+      initialValues={{
+        // Properties we want to capture in our form
         firstName: "",
         lastName: "",
-        phonrNum: "",
+        phoneNum: "",
         email: "",
         agree: false,
         contactType: "",
         feedback: "",
       }}
     >
-      {/* // we render for form component inside the formik, tuli import huko juu kwa imports */}
+      {/* We render the Form component inside Formik */}
       <Form>
-        {/* formgroup  component form reactstrap is used in a similar way as form-group class in bootsrap to group labels, fields, controls, and validation error messages together*/}
-        {/* have a prop of ow which givess them the bootstrap row class */}
+        {/* FormGroup component from reactstrap is used in a similar way as form-group class in Bootstrap to group labels, fields, controls, and validation error messages together */}
+        {/* They have a prop of row which gives them the Bootstrap row class */}
         <FormGroup row>
-        <Label htmlFor='firstName' md='2'>
-                        First Name
-                    </Label>
-                    <Col md='10'>
-                    </Col>
-
+          <Label htmlFor="firstName" md="2">
+            First Name
+          </Label>
+          <Col md="10">
+            <Field
+              name='firstName'
+              placeholder='First Name'
+              className='form-control'
+            />
+          </Col>
         </FormGroup>
         <FormGroup row>
-        <Label htmlFor='lastName' md='2'>
-                        Last Name
-                    </Label>
-                    <Col md='10'>
-                    </Col>
+          <Label htmlFor="lastName" md="2">
+            Last Name
+          </Label>
+          <Col md="10">
+            <Field
+              name='lastName'
+              placeholder='Last Name'
+              className='form-control'
+            />
+          </Col>
         </FormGroup>
         <FormGroup row>
-        <Label htmlFor='phoneNum' md='2'>
-        phone
-                    </Label>
-                    <Col md='10'>
-                    </Col>
+          <Label htmlFor="phoneNum" md="2">
+            Phone
+          </Label>
+          <Col md="10">
+            <Field
+              name='phoneNum'
+              placeholder='Phone'
+              className='form-control'
+            />
+          </Col>
         </FormGroup>
         <FormGroup row>
-        <Label htmlFor='email' md='2'>
-                        email
-                    </Label>
-                    <Col md='10'>
-                    </Col>
+          <Label htmlFor="email" md="2">
+            Email
+          </Label>
+          <Col md="10">
+            <Field
+              name='email'
+              placeholder='Email'
+              type='email'
+              className='form-control'
+            />
+          </Col>
         </FormGroup>
         <FormGroup row>
-        <Label check md= {{size:4, offset:2}}>
-            {/* above means for md and overview ports take up a third of a row cos (4obvs) and we offset it by two columns so that it is positoned further into the right than the other labels*/}
-                       May we contact you?
-                    </Label>
-                    <Col md='4'>
-                    </Col>
+          <Label check md={{ size: 4, offset: 2 }}>
+            {/* For md and above viewports, take up a third of a row (4 columns) and offset it by two columns so that it is positioned further to the right */}
+            <Field
+              name='agree'
+              type='checkbox'
+              className='form-check-input'
+            />
+            {/* {' '}just for the space tu */}
+            {' '}
+            May we contact you?
+          </Label>
+          <Col md="4">
+            <Field
+              name='contactType'
+              as='select'
+              placeholder=''
+              className='form-control'
+            >
+              {/* Options for the select menu */}
+              <option>By Phone</option>
+              <option>By Email</option>
+            </Field>
+          </Col>
         </FormGroup>
         <FormGroup row>
-        <Label htmlFor='feedack' md='2'>
-                        Your Feedback
-                    </Label>
-                    <Col md='10'>
-                    </Col>
+          <Label htmlFor="feedback" md="2">
+            Your Feedback
+          </Label>
+          <Col md="10">
+            <Field
+              name='feedback'
+              as='textarea'
+              rows='12'
+              className='form-control'
+            />
+          </Col>
         </FormGroup>
+        {/* Add a submit button */}
         <FormGroup row>
-            
+          <Col >
+           
+          </Col>
         </FormGroup>
-
       </Form>
     </Formik>
   );
