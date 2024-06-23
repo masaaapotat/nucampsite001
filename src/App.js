169dfa2e449f52c6b421a3825657cd36264234eb
masaapotat <masaakavulanya@gmail.com>
2024-06-23 16:40:16 +0300

    Async Partners

--- src/App.js
+++ src/App.js
@@ -1,44 +1,38 @@
-
 //import React from 'react';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import ContactPage from './pages/ContactPage';
-import CampsiteDetailPage from './pages/CampsiteDetailPage';
-import AboutPage from './pages/AboutPage';
-import { fetchCampsites } from './features/campsites/CampsitesSlice';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ContactPage from "./pages/ContactPage";
+import CampsiteDetailPage from "./pages/CampsiteDetailPage";
+import AboutPage from "./pages/AboutPage";
+import { fetchCampsites } from "./features/campsites/CampsitesSlice";
+import { fetchPartners } from "./features/partners/partnersSlice";
 
 
 function App() {
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(fetchCampsites());
-    }, [dispatch]);
-    return (
-        <div className='App'>
-            <Header />
-            <Routes>
-                <Route path = '/' element = {<HomePage/>}/>
-                <Route path = 'contact' element = {<ContactPage/>} />
-                <Route path = 'directory' element = {<CampsitesDirectoryPage/>}/>
-                <Route
-                    path='directory/:campsiteId'
-                    element={<CampsiteDetailPage />}
-                />
-                <Route
-                    path='about'
-                    element={<AboutPage />}
-                />
-
-            </Routes>
-            <Footer />
-        </div>
-    );
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCampsites());
+    dispatch(fetchPartners());
+  }, [dispatch]);
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="directory" element={<CampsitesDirectoryPage />} />
+        <Route path="directory/:campsiteId" element={<CampsiteDetailPage />} />
+        <Route path="about" element={<AboutPage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
